e04b8b12e2ddc48df4c61aba16519e13270f4f6a
Anatoliy96 <leaved@abv.bg>
2021-01-13 10:03:18 +0200

    Fix payment date and client activity update

--- BillingSystemMVC/BLL/PaymentBLL.cs
+++ BillingSystemMVC/BLL/PaymentBLL.cs
@@ -1,5 +1,7 @@
 ﻿using BillingSystemMVC.DAO;
+using BillingSystemMVC.Migrations;
 using BillingSystemMVC.Model;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -14,6 +16,40 @@ namespace BillingSystemMVC.BLL
         {
             BillingSystemContext context = new BillingSystemContext();
             return context.Payments.Where(p => p.Date >= From && p.Date <= To).ToList();
+        }
+
+        public Payment RegisterPayment([Bind] Payment payment)
+        {
+            try
+            {
+                using (BillingSystemContext context = new BillingSystemContext())
+                {
+                    DateTime validity = context.Clients.FirstOrDefault(c => c.IDNumber == payment.Client).Validity;
+                    Tariff tarif = context.Clients.Include(c => c.ClientTarif).FirstOrDefault(c => c.IDNumber == payment.Client).ClientTarif;
+                    Tariff tarifa = context.Tariffs.FirstOrDefault(t => t.IDNumber == context.Clients.FirstOrDefault(c => c.TariffId == t.IDNumber).TariffId);
+                    decimal tarifPrice = tarifa.Price;
+                    decimal amount = payment.Amount;
+
+                    int months = Convert.ToInt32(amount / tarifPrice);
+
+                    validity = validity.AddMonths(months);
+                    payment.Date = DateTime.Now;
+                    context.Payments.Add(payment);
+
+                    context.Clients.FirstOrDefault(c => c.IDNumber == payment.Client).Validity = validity;
+                    
+                    context.SaveChanges();
+                    return payment;
+
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+            
         }
     }
 }
--- BillingSystemMVC/Controllers/PaymentController.cs
+++ BillingSystemMVC/Controllers/PaymentController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BillingSystemMVC.BLL;
 using BillingSystemMVC.DAO;
 using BillingSystemMVC.Model;
 using Microsoft.AspNetCore.Mvc;
@@ -26,41 +27,11 @@ namespace BillingSystemMVC.Controllers
 
 
         [HttpPost]
-        public IActionResult Register([Bind] Payment payment)
+        public IActionResult Register(Payment payment)
         {
-            try
-            {
-                using (BillingSystemContext context = new BillingSystemContext())
-                {
-                    // Взимаш до кога е валидна
-                    DateTime validity = context.Clients.FirstOrDefault(c => c.IDNumber == payment.Client).Validity;
-                    Tariff tarif = context.Clients.Include(c => c.ClientTarif).FirstOrDefault(c => c.IDNumber == payment.Client).ClientTarif;
-                    Tariff tarifa = context.Tariffs.FirstOrDefault(t => t.IDNumber == context.Clients.FirstOrDefault(c => c.TariffId == t.IDNumber).TariffId);
-                    decimal tarifPrice = tarifa.Price;
-                    decimal amount = payment.Amount;
-
-                    int months = Convert.ToInt32(amount / tarifPrice);
-
-                    validity.AddMonths(months);
-
-                    // Хващаш колко ти е платил и добавяш спрямо сумата дни
-                    // Задаваш
-                    context.Payments.Add(payment);
-                    
-
-                    context.Clients.FirstOrDefault(c => c.IDNumber == payment.Client).Validity = validity;
-
-                    context.SaveChanges();
-                    return RedirectToAction("Index", "Clients");
-                }
-
-            }
-            catch (Exception ex)
-            {
-
-                
-            }
-            return View();
+            PaymentBLL paymentBLL = new PaymentBLL();
+            paymentBLL.RegisterPayment(payment);
+            return RedirectToAction("Index", "Clients");
         }
     }
 
