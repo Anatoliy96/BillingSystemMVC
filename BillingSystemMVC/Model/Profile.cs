616507b7d57489691be40304024dddce13703a0a
Anatoliy96 <leaved@abv.bg>
2021-01-21 17:03:06 +0200

    fixing user login

--- BillingSystemMVC/Model/Profile.cs
+++ BillingSystemMVC/Model/Profile.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,8 @@ namespace BillingSystemMVC.Model
     {
         public string UserName { get; set; }
         public string UserNameType { get; set; }
+        
+        [DataType(DataType.Password)]
         public string Password { get; set; }
         public string PhoneNumber { get; set; }
         public string Name { get; set; }
